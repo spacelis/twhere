bcd02f806ff0dc9ebf76576fc3f0fa079d4a32de
wenli <spacelis@gmail.com>
2013-02-11 10:29:19 +0100

    Code style improvement

--- config.py
+++ config.py
@@ -15,32 +15,32 @@ import site
 site.addsitedir('../model')
 
 from model.colfilter import CosineSimilarity
-from model.colfilter import HistoryDamper
 from model.colfilter import LinearCombination
 from model.colfilter import gaussian_pdf
 from model.colfilter import exponent_pdf
 
 HISTDAMP_PARAM_DEFAULT = {"veclen": 100,
-                  "interval": (0., 24 * 3600.),
-                  "damp_func": exponent_pdf,
-                  "params": {'l': 1 / 3600.}}
+                          "interval": (0., 24 * 3600.),
+                          "damp_func": exponent_pdf,
+                          "params": {'l': 1 / 3600.}}
 
 VECTORDB_PARAM_DEFAULT = {"simnum": 20,
-                  "similarity": CosineSimilarity([]),
-                  "aggregator": LinearCombination()}
+                          "similarity": CosineSimilarity([]),
+                          "aggregator": LinearCombination()}
 
 VECTORIZOR_PARAM_DEFAULT = {"veclen": 100,
-                    "interval": (0., 24 * 3600.),
-                    "kernel": gaussian_pdf,
-                    "params": (3600.,),
-                    "isaccum": False,
-                    "normalized": False}
+                            "interval": (0., 24 * 3600.),
+                            "kernel": gaussian_pdf,
+                            "params": (3600.,),
+                            "isaccum": False,
+                            "normalized": False}
 
 HISTDAMP_PARAM = dict(HISTDAMP_PARAM_DEFAULT)
 
 VECTORDB_PARAM = dict(VECTORDB_PARAM_DEFAULT)
 
 VECTORIZOR_PARAM = dict(VECTORIZOR_PARAM_DEFAULT)
+
 
 def reset_config():
     """ reset the configuration

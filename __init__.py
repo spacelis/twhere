687430fdbf5df6e079127a542915573952e22d8f
wenli <spacelis@gmail.com>
2013-02-11 23:52:56 +0100

    Make current dir as a module

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+File: __init__.py
+Author: SpaceLis
+Changes:
+    0.0.1 The first version
+Description:
+"""
+__version__ = '0.0.1'
+
+
+import site
+site.addsitedir('.')

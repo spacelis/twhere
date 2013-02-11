c5df25c7f1ca312d93c874594b5becbdaa46b5dd
wenli <spacelis@gmail.com>
2013-02-11 23:50:32 +0100

    Add RandomData()

--- dataprov.py
+++ dataprov.py
@@ -14,7 +14,8 @@ __version__ = '0.0.1'
 import logging
 LOGGER = logging.getLogger(__name__)
 
-from datetime import datetime
+from datetime import datetime, date, timedelta
+import numpy as NP
 
 
 class MySQLData(object):
@@ -32,8 +33,8 @@ class MySQLData(object):
             idname = 'id'
             colname = 'category'
         self.conn = sql.connect(user='root',
-            read_default_file='/home/wenli/devel/mysql/my.cnf',
-            db='geotweet')
+                                read_default_file='/home/wenli/devel/mysql/my.cnf',
+                                db='geotweet')
         cur = self.conn.cursor(cursorclass=sql.cursors.DictCursor)
         cur.execute('select distinct %s as id from category' % (idname,))
         self.namespace = [c['id'] for c in cur]
@@ -48,8 +49,8 @@ class MySQLData(object):
                                     on c.pid = p.id
                                 where c.isbot is null and p.city='%s'
                                 order by c.uid, c.created_at''' %
-                                (colname, 'checkins_6', self.city))
-        LOGGER.info('Total Checkins: %d' % (total,))
+                            (colname, 'checkins_6', self.city))
+        LOGGER.info('Total Checkins: %d' % (total, ))
         self.data = list(cur)
 
     def get_namespace(self):
@@ -81,6 +82,41 @@ class TextData(object):
         with open(nspath) as fin:
             for line in fin:
                 self.namespace.append(line.strip())
+
+    def get_namespace(self):
+        """ Return a list of unique labels in the predicting field
+        """
+        return self.namespace
+
+    def get_data(self):
+        """ Return a list of checkins
+        """
+        return self.data
+
+
+class RandomData(object):
+    """ Data provider from text files
+    """
+    def __init__(self, nsfile, length=3, number=3000):
+        super(RandomData, self).__init__()
+        nspath = 'data/%s_ns.table' % (nsfile,)
+        LOGGER.info('Random data provider: length=' + str(length) + ', ' + nspath)
+        self.namespace = list()
+        with open(nspath) as fin:
+            for line in fin:
+                self.namespace.append(line.strip())
+
+        self.data = list()
+        lengths = NP.ceil(NP.random.standard_exponential(number))
+        print lengths
+        for idx, length in enumerate(lengths):
+            for _ in range(int(length)):
+                tr_id = str(idx)
+                poi = self.namespace[NP.random.randint(len(self.namespace))]
+                tick = str(datetime.fromordinal(date.today().toordinal()) + timedelta(seconds=NP.random.randint(3600 * 24)))
+                pid = str(NP.random.randint(10000))
+                tmp = {'trail_id': tr_id, 'poi': poi, 'tick': datetime.strptime(tick, '%Y-%m-%d %H:%M:%S'), 'pid': pid}
+                self.data.append(tmp)
 
     def get_namespace(self):
         """ Return a list of unique labels in the predicting field

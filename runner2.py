a45765efba915c50da9bf23f0db52fcb4bdaca3b
wenli <spacelis@gmail.com>
2013-03-23 18:33:47 +0100

    Use Json instead of yaml for logging configuration

--- runner2.py
+++ runner2.py
@@ -10,64 +10,67 @@ Description:
 """
 __version__ = '0.0.1'
 
-import sys
-import os
-import yaml
 import json
 import logging
 import logging.config
+import argparse
 from twhere.exprmodels import experiment
 from twhere.config import Configuration
 
 CITY = dict(zip(['NY', 'CH', 'LA', 'SF'],
            ['27485069891a7938', '1d9a5370a355ab0c', '3b77caf94bfc81fe', '5a110d312052166f']))
 
-LOGGING_CONF = """
-version: 1
-formatters:
-    simple:
-        format: "%(asctime)s %(name)s [%(levelname)s] %(message)s"
+LOGGING_CONF = {'version': 1,
+                'formatters': {
+                    'simple': {'format': "%(asctime)s %(name)s [%(levelname)s] %(message)s"}
+                },
+                'handlers': {
+                    'console': {'class': 'logging.StreamHandler',
+                                'level': 'INFO',
+                                'formatter': 'simple',
+                                'stream': 'ext://sys.stderr'}
+                },
 
-handlers:
-    console:
-        class: logging.StreamHandler
-        level: INFO
-        formatter: simple
-        stream: ext://sys.stderr
-
-root:
-    level: INFO
-    handlers: [console, info_file_handler]
-"""
+                'root': {
+                    'level': 'INFO',
+                    'handlers': ['console', ]
+                }
+}
 
 
-def setup_logging(logstring):
+def setup_logging(logconf):
     """ Setup logging
     """
-    logconf = yaml.load(logstring)
     logging.config.dictConfig(logconf)
 
 
+def parse_parameter():
+    """ Parse the argument
+    """
+    parser = argparse.ArgumentParser(description='Running Trail Prediction')
+    parser.add_argument('-f', dest='conffile', action='store', metavar='FILE', default=None,
+            help='Running with delta configuration from the FILE')
+    parser.add_argument('-s', dest='confstr', action='store', metavar='JSON', default=None,
+            help='Running with the delta configuration from the json string')
+    args = parser.parse_args()
+    return args
+
 if __name__ == '__main__':
-    if len(sys.argv) < 3:
-        print >> sys.stderr, 'No folder provided for results'
-        print >> sys.stderr, 'Usage: runtest.py <resdir> <conf>'
-        sys.exit(-1)
-    resdir = sys.argv[1]
-    conffile = sys.argv[2]
-    if not os.path.isdir(resdir):
-        os.mkdir(resdir)
     setup_logging(LOGGING_CONF)
     LOGGER = logging.getLogger(__name__)
     LOGGER.debug('DEBUG is enabled')
     try:
         import resource
-        resource.setrlimit(resource.RLIMIT_AS, (2.5 * 1024 * 1024 * 1024L, -1L))
+        resource.setrlimit(resource.RLIMIT_AS, (800 * 1024 * 1024L, -1L))
     except ValueError as err:
         LOGGER.warn('Failed set resource limits. Because {0}'.format(err.message))
 
-    with open(conffile) as fconf:
-        dconf = json.loads(fconf.read())
+    appargs = parse_parameter()
+    if appargs.conffile is not None:
+        with open(appargs.conffile) as fconf:
+            dconf = json.loads(fconf.read())
+    if appargs.confstr is not None:
+        dconf = json.loads(appargs.confstr)
     conf = Configuration()
     conf.update(dconf)
     conf['expr.city.id'] = CITY[conf['expr.city.name']]
